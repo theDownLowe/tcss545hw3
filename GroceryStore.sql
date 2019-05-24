1d3a3fb2cafcc5ddb1489c8f2aa7dcb16c761916
Trevor N. Lowe <tnlral@uw.edu>
2019-05-24 11:09:58 -0700

    Added Keys and updated data types

--- GroceryStore.sql
+++ GroceryStore.sql
@@ -7,64 +7,77 @@ USE `Grocery Store`;
 -- database schemas
 
 
--- Inventory(ItemId, Name, Description, DepartmentId, DistributorId, CurrentPrice, InventoryCount)
-CREATE TABLE Inventory( 
-  ItemId           INT  NOT NULL, 
-  Name             VARCHAR(15) NOT NULL, 
-  Description      VARCHAR(20)  NOT NULL, 
-  DepartmentId     INT NOT NULL,
-  DistributorId    INT NOT NULL,
-  CurrentPrice     INT NOT NULL,
-  InventoryCount   INT   );
-
-
--- Employee(EmployeeId, Name, Phone, Address, DoB, HireDate, Position, DepartmentId, Salary)
-CREATE TABLE Employee( 
-  EmployeeId  INT  NOT NULL, 
-  Name        VARCHAR (15)  NOT NULL, 
-  Phone       VARCHAR   NOT NULL, 
-  Address     VARCHAR (20) 	NOT NULL ,
-  DoB         Date   NOT NULL, 
-  HireDate    Date NOT NULL,
-  Position    VARCHAR (5) NOT NULL,
-  DepartmentId INT NOT NULL ,
-  Salary       INT );
-
-
-  -- Customer(CustomerId, Name, Phone, Address, DoB, CustomerSince, RewardPoints)
+-- Customer(CustomerId, Name, Phone, Address, DoB, CustomerSince, RewardPoints)
 CREATE TABLE Customer( 
-  CustomerId    INT  NOT NULL, 
-  Name          VARCHAR (10)    NOT NULL, 
-  Phone         VARCHAR   NOT NULL, 
-  Address       VARCHAR(20) 	NOT NULL ,
-  DoB           Date  NOT NULL,
-  CustomerSince Date   NOT NULL  
-  RewardPoints INT  );
+  CustomerId    INT NOT NULL PRIMARY KEY, 
+  Name          VARCHAR(60) NOT NULL, 
+  Phone         CHAR(10) NOT NULL, 
+  Address       VARCHAR(120) NOT NULL ,
+  DoB           Date NOT NULL,
+  CustomerSince Date NOT NULL,  
+  RewardPoints  INT DEFAULT 0,
+);
 
 
 -- Department(DepartmentId, Name)
 CREATE TABLE Department ( 
-  DepartmentId  INT  NOT NULL, 
-  Name   VARCHAR (10)  NOT NULL );
+  DepartmentId  INT NOT NULL PRIMARY KEY, 
+  Name          VARCHAR(60) NOT NULL 
+);
 
 
 -- Distributor(DistributorId, Name, Phone, Address, PrimaryContactName)
 CREATE TABLE Distributor( 
-  DistributorId          INT  NOT NULL, 
-  Name                     VARCHAR (10)    NOT NULL, 
-  Phone                    VARCHAR   NOT NULL, 
-  Address                  VARCHAR (20)   NOT NULL ,
-  PrimaryContactName        VARCHAR (20) NOT NULL );
+  DistributorId       INT NOT NULL PRIMARY KEY, 
+  Name                VARCHAR (60) NOT NULL, 
+  Phone               CHAR(10) NOT NULL, 
+  Address             VARCHAR(120) NOT NULL,
+  PrimaryContactName  VARCHAR(60) NOT NULL
+);
+
+
+-- Inventory(ItemId, Name, Description, DepartmentId, DistributorId, CurrentPrice, InventoryCount)
+CREATE TABLE Inventory( 
+  ItemId           INT  NOT NULL PRIMARY KEY, 
+  Name             VARCHAR(60) NOT NULL, 
+  Description      VARCHAR(255) NOT NULL, 
+  DepartmentId     INT NOT NULL,
+  DistributorId    INT NOT NULL,
+  CurrentPrice     DECIMAL(7,2) DEFAULT 0,
+  InventoryCount   INT DEFAULT 0,
+  FOREIGN KEY (DepartmentId) REFERENCES Department(DepartmentId),
+  FOREIGN KEY (DistributorId) REFERENCES Distributor(DistributorId)
+);
+
+
+-- Employee(EmployeeId, Name, Phone, Address, DoB, HireDate, Position, DepartmentId, Salary)
+CREATE TABLE Employee( 
+  EmployeeId    INT  NOT NULL PRIMARY KEY, 
+  Name          VARCHAR(60) NOT NULL, 
+  Phone         CHAR(10) NOT NULL, 
+  Address       VARCHAR(120)	NOT NULL ,
+  DoB           Date NOT NULL, 
+  HireDate      Date NOT NULL,
+  Position      VARCHAR(60) NOT NULL,
+  DepartmentId  INT NOT NULL,
+  Salary        DECIMAL(8,2),
+  FOREIGN KEY (DepartmentId) REFERENCES Department(DepartmentId)
+);
+
 
 -- Purchase(PurchaseId, EmployeeId, CustomerId, ItemId, CostPerUnit, Quantity, TotalCost)
 CREATE TABLE Purchase( 
-  PurchaseId          INT  NOT NULL, 
-  EmployeeId          INT    NOT NULL, 
-  CustomerId          INT NOT NULL, 
-  ItemId              INT  NOT NULL ,
-  CostPerUnit        INT NOT NULL;
-  Quantity           INT NOT NULL; 
-  TotalCost          INT NOT NULL ) ;
+  PurchaseId    INT NOT NULL PRIMARY KEY, 
+  EmployeeId    INT NOT NULL, 
+  CustomerId    INT NOT NULL, 
+  ItemId        INT NOT NULL,
+  CostPerUnit   DECIMAL(7,2) NOT NULL,
+  Quantity      INT NOT NULL, 
+  TotalCost     DECIMAL(8,2) NOT NULL,
+  FOREIGN KEY (EmployeeId) REFERENCES Employee(EmployeeId),
+  FOREIGN KEY (CustomerId) REFERENCES Customer(CustomerId),
+  FOREIGN KEY (ItemId) REFERENCES Inventory(InventoryId)
+);
 
 
 
